3cbbeb773fce89f2b7df8b78bdfdd35305b89c67
svlandeg <sofie.vanlandeghem@gmail.com>
2019-01-07 15:13:29 +0100

    using pyconll package

--- src/be/svlandeg/packeval/io/read_ud.py
+++ src/be/svlandeg/packeval/io/read_ud.py
@@ -1,5 +1,8 @@
 import os
 
+import pyconll
+import pyconll.util
+
 # C:\Users\Sofie\Documents\data\UD_2_3\ud-treebanks-v2.3\UD_English-EWT
 ud_location = os.path.join('C:', os.sep, 'Users', 'Sofie', 'Documents', 'data', 'UD_2_3', 'ud-treebanks-v2.3')
 
@@ -15,9 +18,28 @@ def read_english_training():
     return data
 
 
+def read_annotations():
+    ud_eng = 'UD_English-EWT'
+    ud_eng_path = os.path.join(ud_location, ud_eng)
+    train_conllu = os.path.join(ud_eng_path, 'en_ewt-ud-train.conllu')
+
+    train = pyconll.load_from_file(train_conllu)
+    for sentence in train:
+        print(sentence.id)
+        for token in sentence:
+            # token.upos, token.xpos, token.feats, token.head, token.deps, token.misc
+            print(token.id, token.form, '\t\t\t -->', token.lemma)
+        print()
+
+
 if __name__ == '__main__':
     print()
-    txt_train = read_english_training()
-    print(txt_train)
+
+    # READING RAW TEXT
+    # txt_train = read_english_training()
+    # print(txt_train)
+
+    # READING ANNOTATIONS
+    read_annotations()
 
 
