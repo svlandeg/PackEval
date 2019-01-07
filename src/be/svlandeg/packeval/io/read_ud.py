f8e0260c90237b1663a3019759097147ae7c56d3
svlandeg <sofie.vanlandeghem@gmail.com>
2019-01-07 15:57:57 +0100

    parsing raw conllu format to identify compound tokens

--- src/be/svlandeg/packeval/io/read_ud.py
+++ src/be/svlandeg/packeval/io/read_ud.py
@@ -27,9 +27,42 @@ def read_annotations():
     for sentence in train:
         print(sentence.id)
         for token in sentence:
-            # token.upos, token.xpos, token.feats, token.head, token.deps, token.misc
+            # token.upos, token.xpos, token.feats, token.head, token.deps, token.deprel, token.misc
             print(token.id, token.form, '\t\t\t -->', token.lemma)
         print()
+
+
+def read_raw_annotations():
+    ud_eng = 'UD_English-ParTUT'
+    ud_eng_path = os.path.join(ud_location, ud_eng)
+    train_conllu = os.path.join(ud_eng_path, 'en_partut-ud-train.conllu')
+
+    with open(train_conllu, 'r', encoding='utf-8') as fin:
+        for line in fin:
+            pure_text = line.rstrip('\n')
+            is_comment = line.startswith("#")
+            is_error_comment = line.startswith("# error_annotation =")
+            is_doc = line.startswith("# newdoc")
+            is_doc_id = line.startswith("# newdoc id = ")
+            is_text = line.startswith("# text = ")
+            is_sentence = line.startswith("# sent_id = ")
+            if is_comment and not is_text and not is_sentence and not is_doc and not is_error_comment:
+                print("Found weird comment line: ", pure_text)
+            if is_doc:
+                doc_id = pure_text.replace("# newdoc id = ", "")
+            if is_text:
+                text = pure_text.replace("# text = ", "")
+            if is_sentence:
+                sent_id = pure_text.replace("# sent_id = ", "")
+            if not is_comment:
+                word = pure_text.split('\t')
+                # print(pure_text)
+                # print(word)
+                # print()
+                if len(pure_text) > 0:
+                    word_index = word[0]
+                    if '-' in word_index:
+                        print(word)
 
 
 if __name__ == '__main__':
@@ -40,6 +73,7 @@ if __name__ == '__main__':
     # print(txt_train)
 
     # READING ANNOTATIONS
-    read_annotations()
+    # read_annotations()
+    read_raw_annotations()
 
 
